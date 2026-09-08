50d0ae994c4bd65961f4d937a2729af5876e0f7a
zzzZoriX <zorix228337@gmail.com>
2026-09-09 02:07:35 +0300

    add ui to core as service

--- src/core.cpp
+++ src/core.cpp
@@ -5,4 +5,17 @@ void tn_core::run(std::string_view cfgfp) {
     tn_cfg = handlers::config(cfgfp);
 
     tn_slm = handlers::servers(tn_cfg.get_servers_file());
+    tn_ui = ui::UI(std::make_shared<tn_core>(this));
+}
+
+const handlers::servers* const tn_core::get_servers_list_service() {
+    return &tn_slm;
+}
+
+const ui::UI* const tn_core::get_ui_service() {
+    return &tn_ui;
+}
+
+const handlers::config* const tn_core::get_config_service() {
+    return &tn_cfg;
 }
--- src/core.hpp
+++ src/core.hpp
@@ -8,11 +8,14 @@
 #include <string_view>
 #include <memory>
 #include <tn-handlers/handlers.hpp>
+#include <tn-ui/ui.hpp>
 
 
 // As long as Trixy is not asynchronous, there is no need to use a mutex or `boost::asio::strand`.
-class tn_core {
+class tn_core: public std::enable_shared_from_this<tn_core> {
     handlers::config tn_cfg;    // trixy-net config
+
+    ui::UI tn_ui;               // trixy-net user interface
     
     handlers::servers tn_slm;   // trixy-net servers list manager
     // network services...
@@ -26,4 +29,9 @@ public:
     void restart();
 
     void stop();
+
+
+    const handlers::servers* const get_servers_list_service();
+    const ui::UI* const get_ui_service();
+    const handlers::config* const get_config_service();
 };
