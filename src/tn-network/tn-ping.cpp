0da531d220718fe585c43acb2eafff8a59c5a890
zzzZoriX <zorix228337@gmail.com>
2026-09-12 02:30:03 +0300

    complete the trixy-ping

---
+++ src/tn-network/tn-ping.cpp
@@ -0,0 +1,113 @@
+#include "tn-ping.hpp"
+#include <iostream>
+
+using namespace network;
+
+ping_manager::ping_manager(io_context& ioc)
+:   ioc(ioc) {}
+
+void ping_manager::start(std::vector<std::string> hosts, ping_callback callback) const {
+    for(const auto& host: hosts)
+        std::make_shared<pinger>(host, ioc, callback)->start();
+}
+
+
+pinger::pinger(const std::string& host, io_context& ioc, ping_callback callback)
+:   host(host)
+,   resolver(ioc)
+,   ioc(ioc)
+,   sock(ioc, ip::icmp::v4())
+,   timer(ioc)
+,   seq_num(0)
+,   timeout(5) {
+    dest = *resolver.resolve(ip::icmp::v4(), host, "").begin();
+}
+
+void pinger::start() {
+    start_send();
+    start_receive();
+}
+
+void pinger::start_send() {
+    std::string body{"trixy-ping"};
+
+    icmp_header echo_req;
+
+    echo_req.type(icmp_header::echo_request);
+    echo_req.code(0);
+    echo_req.id(get_id());
+    echo_req.seq_num(++seq_num);
+
+    compute_check_sum(echo_req, body.begin(), body.end());
+
+    streambuf req_buffer;
+
+    std::ostream os{&req_buffer};
+    os << echo_req << body;
+
+    time_sent = std::chrono::steady_clock::now();
+
+    sock.send_to(
+        req_buffer.data(),
+        dest
+    );
+
+    timer.expires_after(timeout);
+    timer.async_wait([this, self = shared_from_this()] (system::error_code ec) {
+        handle_timeout(ec);
+    });
+}
+
+void pinger::handle_timeout(system::error_code ec) {
+    if(ec) {
+        // todo: add aborting there
+    }
+    else {
+        system::error_code sock_ec;
+
+        sock.cancel(sock_ec);
+        sock.close(sock_ec);
+
+        callback(ERROR_PING(host));
+    }
+}
+
+void pinger::start_receive() {
+    sock.async_receive(
+        buffer.prepare(65536), // 64KB
+        [this, self = shared_from_this()] (system::error_code ec, std::size_t len) {
+            handle_receive(ec, len);
+        }
+    );
+}
+
+void pinger::handle_receive(system::error_code ec, std::size_t len) {
+    if(ec) {
+        // todo: add aboting there
+    }
+    else {
+        buffer.commit(len);
+
+        std::istream is{&buffer};
+
+        icmp_header icmp_head;
+        ipv4_header ipv4_head;
+
+        is >> ipv4_head >> icmp_head;
+
+        if(
+            is  &&
+            icmp_head.type() == icmp_header::echo_reply   &&
+            icmp_head.id() == get_id()  &&
+            icmp_head.seq_num() == seq_num
+        ) {
+            auto now{std::chrono::steady_clock::now()};
+
+            auto elapsed{
+                std::chrono::duration_cast<ping_time>(now - time_sent)
+            };
+
+            callback(ping_result(elapsed, host));
+        }
+    }
+}
