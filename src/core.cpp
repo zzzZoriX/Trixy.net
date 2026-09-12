1319994bc139cfdc9931bc2e538c6482464ef1b4
zzzZoriX <zorix228337@gmail.com>
2026-09-12 11:45:28 +0300

    add ping manager to core class and all function for work with it

--- src/core.cpp
+++ src/core.cpp
@@ -1,12 +1,13 @@
 #include "core.hpp"
 
 
-void tn_core::run(std::string_view cfgfp) {
+void tn_core::run(std::string_view cfgfp, boost::asio::io_context& ioc) {
     tn_cfg = handlers::config(cfgfp);
 
     tn_slm = handlers::servers(tn_cfg.get_servers_file());
-    tn_ui = ui::UI(std::make_shared<tn_core>(this));
+    tn_ui = ui::UI(shared_from_this());
     tn_loger = error_handling::loger(tn_cfg.get_logs_path());
+    tn_pm = network::ping_manager(ioc, shared_from_this());
 }
 
 void tn_core::stop() {
@@ -27,4 +28,8 @@ handlers::config* const tn_core::get_config_service() {
 
 error_handling::loger* const tn_core::get_loger() {
     return &tn_loger;
+}
+
+network::ping_manager* const tn_core::get_ping_manager() {
+    return &tn_pm;
 }
--- src/core.hpp
+++ src/core.hpp
@@ -10,6 +10,8 @@
 #include <tn-handlers/handlers.hpp>
 #include <tn-ui/ui.hpp>
 #include <tn-error_handling/loger.hpp>
+#include <tn-network/tn-ping.hpp>
+#include <boost/asio.hpp>
 
 
 // As long as Trixy is not asynchronous, there is no need to use a mutex or `boost::asio::strand`.
@@ -20,13 +22,13 @@ class tn_core: public std::enable_shared_from_this<tn_core> {
     
     handlers::servers tn_slm;   // trixy-net servers list manager
     error_handling::loger tn_loger;
-    // network services...
+    network::ping_manager tn_pm;    // trixy-net ping manager
     // settings
 
 public:
     tn_core() = default;
 
-    void run(std::string_view cfgfp);
+    void run(std::string_view cfgfp, boost::asio::io_context& ioc);
 
     void restart();
 
@@ -36,4 +38,5 @@ public:
     ui::UI* const get_ui_service();
     handlers::config* const get_config_service();
     error_handling::loger* const get_loger();
+    network::ping_manager* const get_ping_manager();
 };
