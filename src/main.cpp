a61b94a713a5447d03d58d799fcaf1910b6884fe
zzzZoriX <zorix228337@gmail.com>
2026-09-12 12:00:40 +0300

    add boost::asio::io_context for core->run()

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "core.hpp"
+#include <boost/asio.hpp>
 
 
 int main(void) {
-    auto core{std::make_shared<tn_core>()};
+    try {
+        boost::asio::io_context ioc;
 
-    core->run("../examples/config.cfg");
+        auto core{std::make_shared<tn_core>()};
+
+        core->run("../examples/config.cfg", ioc);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+
+        return 1;
+    }
 
     return 0;
 }
