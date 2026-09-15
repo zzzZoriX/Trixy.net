973bb8843d7da0950691621d7714d2ac5e35a059
zzzZoriX <zorix228337@gmail.com>
2026-09-15 11:50:10 +0300

    add log date to log file name

--- src/tn-error_handling/loger.cpp
+++ src/tn-error_handling/loger.cpp
@@ -22,11 +22,10 @@ void loger::add_log(std::string msg, const error_code code) {
 }
 
 void loger::create_log() const {
-    auto now{std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now())};
+    auto local_time{std::chrono::current_zone()->to_local(std::chrono::system_clock::now())};
 
-    auto str_time{std::format("{:%T}", now)};
 
-    std::ofstream log_fp{std::format("{}tn-log::{}.log", logs_path, str_time)};
+    std::ofstream log_fp{std::format("{}tn-log::{:%Y-%m-%d::%H:%M:%S}.log", logs_path, local_time)};
 
     if(!log_fp.is_open()) {
         std::cerr << "CAN'T CREATE THE LOG FILE. ERROR." << std::endl;
